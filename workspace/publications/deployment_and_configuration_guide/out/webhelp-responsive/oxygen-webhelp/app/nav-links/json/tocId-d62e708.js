76fbc34c234fff7512ad98fadbb43e36f8470b6e
Pat Scully <patscully@EPL.IE>
2019-11-19 16:33:35 +0000

    fixedglossterm

--- workspace/publications/deployment_and_configuration_guide/out/webhelp-responsive/oxygen-webhelp/app/nav-links/json/tocId-d62e708.js
+++ workspace/publications/deployment_and_configuration_guide/out/webhelp-responsive/oxygen-webhelp/app/nav-links/json/tocId-d62e708.js
@@ -1,1 +1,1 @@
-define({"topics" : [{"title":"Connector API SDK","href":"topics\/glossary\/deployment_glossary_9.html#IBM_StoredIQ_Insights","attributes": {"data-id":"IBM_StoredIQ_Insights",},"menu": {"hasChildren":false,},"tocID":"IBM_StoredIQ_Insights-d62e709","topics":[]}]});
+define({"topics" : [{"title":"IBM_StoredIQ_Insights","href":"topics\/glossary\/deployment_glossary_9.html#IBM_StoredIQ_Insights","attributes": {"data-id":"IBM_StoredIQ_Insights",},"menu": {"hasChildren":false,},"tocID":"IBM_StoredIQ_Insights-d62e709","topics":[]}]});
